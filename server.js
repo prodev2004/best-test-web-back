2dcc51b663f0c16c7a74862d2d4b9d8c0f91d96c
Elvis Murseli <82672349+prodev2004@users.noreply.github.com>
2025-03-13 00:54:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors");
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-
-// Proxy route to fetch images
 app.get("/proxy-image", async (req, res) => {
     try {
         const imageUrl = req.query.url;
@@ -17,7 +13,6 @@ app.get("/proxy-image", async (req, res) => {
 
         const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
 
-        // Set correct content type for images
         res.setHeader("Content-Type", response.headers["content-type"]);
         res.send(response.data);
     } catch (error) {
@@ -25,19 +20,4 @@ app.get("/proxy-image", async (req, res) => {
     }
 });
 
-app.get("/fetch-product", async (req, res) => {
-    try {
-        const productUrl = req.query.url;
-        if (!productUrl) {
-            return res.status(400).send("Product URL is required");
-        }
-
-        const response = await axios.get(productUrl, { headers: { "User-Agent": "Mozilla/5.0" } });
-
-        res.send(response.data);
-    } catch (error) {
-        res.status(500).send("Error fetching product data");
-    }
-});
-
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
